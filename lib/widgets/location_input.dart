77feab18e8f1f3c33c4b68b800c4b5398a25704d
Meshooo <mahmoudabdaltwab57@gmail.com>
2025-04-25 02:18:42 +0300

    Adding some dependancies related to the location input screen and handling 90% of getting current location and starting on the map screen to letting the user  picking a locatoin

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,9 @@
+import 'package:favorite_places/screens/map_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geocoding/geocoding.dart' as geocoding;
+import 'package:location/location.dart';
+import 'package:latlong2/latlong.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -8,15 +13,107 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  LatLng? _pickedLocation;
+  bool isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    setState(() {
+      isGettingLocation = true;
+    });
+
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    locationData = await location.getLocation();
+    List<geocoding.Placemark> placemarks = await geocoding
+        .placemarkFromCoordinates(52.2165157, 6.9437819);
+    print(placemarks[0].street);
+    setState(() {
+      isGettingLocation = false;
+      _pickedLocation = LatLng(locationData.latitude!, locationData.longitude!);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget content = Text(
-      "No Location Chosen Yet.",
-      textAlign: TextAlign.center,
-      style: TextTheme.of(
-        context,
-      ).bodyLarge!.copyWith(color: Theme.of(context).colorScheme.onSurface),
-    );
+    Widget content =
+        isGettingLocation == true
+            ? CircularProgressIndicator()
+            : (_pickedLocation == null)
+            ? Text(
+              "No Location Chosen Yet.",
+              textAlign: TextAlign.center,
+              style: TextTheme.of(context).bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            )
+            : ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: FlutterMap(
+                options: MapOptions(
+                  initialCenter: _pickedLocation!,
+                  initialZoom: 15,
+                  onTap: _onMapTapped,
+                  interactionOptions: InteractionOptions(
+                    flags: InteractiveFlag.none,
+                  ),
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        'https://{s}.google.com/vt/lyrs=m&hl={hl}&x={x}&y={y}&z={z}',
+                    additionalOptions: const {'hl': 'ar'},
+
+                    userAgentPackageName: 'com..app',
+                    subdomains: const ['mt0', 'mt1', 'mt2', 'mt3'],
+                  ),
+                  // MarkerLayer(
+                  //   markers: [
+                  //     Marker(
+                  //       point: _pickedLocation!,
+                  //       child: Icon(Icons.location_on),
+                  //     ),
+                  //   ],
+                  // ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: const Icon(
+                      Icons.location_on,
+                      size: 35,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(0, -0.35),
+                    child: Text(
+                      "Your current Location",
+                      style: TextTheme.of(context).titleMedium!.copyWith(
+                        color: const Color.fromARGB(255, 0, 108, 197),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
 
     return Column(
       children: [
@@ -38,7 +135,9 @@ class _LocationInputState extends State<LocationInput> {
           children: [
             TextButton.icon(
               icon: Icon(Icons.location_on, size: 24),
-              onPressed: () {},
+              onPressed: () {
+                _getCurrentLocation();
+              },
               label: Text("Get current Location"),
             ),
             Text(
@@ -50,12 +149,21 @@ class _LocationInputState extends State<LocationInput> {
             ),
             TextButton.icon(
               icon: Icon(Icons.map, size: 24),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => MapScreen()));
+              },
               label: Text("Pick a Location"),
             ),
           ],
         ),
       ],
     );
+  }
+
+  void _onMapTapped(TapPosition tapPosition, LatLng point) {
+    _pickedLocation = point;
+    setState(() {});
   }
 }
