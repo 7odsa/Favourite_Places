c6f1b5bcbdd6d9872e229b5b9267ae3a58361825
Meshooo <mahmoudabdaltwab57@gmail.com>
2025-04-24 18:37:36 +0200

    SSS

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -10,7 +10,6 @@ class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Test
       // test test
       appBar: AppBar(
         title: Text("Great Places"),
